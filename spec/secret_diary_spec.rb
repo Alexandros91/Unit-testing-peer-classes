e5eab90bb987349a974e21de6f66d5cf96cd9387
Rippling Admin <ripplingadmin@alexanders-mbp.home>
2022-11-23 17:04:45 +0000

    Refactoring

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -3,14 +3,14 @@ require 'secret_diary'
 RSpec.describe SecretDiary do
   describe '#read' do
     it 'initially is unlocked and returns the contents' do
-      diary = double :fake_diary, contents: "my contents", read: "my contents"
+      diary = double :fake_diary, read: "fake contents"
       secret_diary = SecretDiary.new(diary)
-      expect(secret_diary.read).to eq "my contents"
+      expect(secret_diary.read).to eq "fake contents"
     end
 
     context 'if the diary is locked' do
       it 'raises error' do
-        diary = double :fake_diary, contents: "my contents", read: "my contents"
+        diary = double :fake_diary
         secret_diary = SecretDiary.new(diary)
         secret_diary.lock
         expect { secret_diary.read }.to raise_error "Go away!"
@@ -19,11 +19,11 @@ RSpec.describe SecretDiary do
 
     context 'if the diary is unlocked' do
       it 'returns the contents' do
-        diary = double :fake_diary, contents: "my contents", read: "my contents"
+        diary = double :fake_diary, read: "fake contents"
         secret_diary = SecretDiary.new(diary)
         secret_diary.lock
         secret_diary.unlock
-        expect(secret_diary.read).to eq "my contents"
+        expect(secret_diary.read).to eq "fake contents"
       end
     end
   end
